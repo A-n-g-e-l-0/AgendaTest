f24cd963f42c7713dfd32f2604c09efd73cbb6f2
10039538 <10039538@GSGZG8Q13D01LE>
2024-10-08 16:52:22 -0600

    Init|Begig

---
+++ Modelos/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace AgendaTest.Modelos
+{
+    public class Persona
+    {
+        public int Id { get; set; } // Identificador único
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Email { get; set; }
+    }
+}
